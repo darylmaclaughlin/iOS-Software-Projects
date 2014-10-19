fd23471f71a4337fbdbc1c7acdaf8af1a924a536
darylmaclaughlin <darylmaclaughlin@gmail.com>
2014-10-19 12:51:50 -0700

    Initial Commit

---
+++ Utility/Utility/Utility.h
@@ -0,0 +1,13 @@
+//
+//  Utility.h
+//  Utility
+//
+//  Created by Daryl Freeman on 10/19/14.
+//  Copyright (c) 2014 Daryl MacLaughlin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Utility : NSObject
+
+@end
---
+++ Utility/Utility/Utility.m
@@ -0,0 +1,13 @@
+//
+//  Utility.m
+//  Utility
+//
+//  Created by Daryl Freeman on 10/19/14.
+//  Copyright (c) 2014 Daryl MacLaughlin. All rights reserved.
+//
+
+#import "Utility.h"
+
+@implementation Utility
+
+@end
